7a3488e341f7189cc93257726281baafa357051b
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-06-25 18:19:41 +0200

    config.py

---
+++ config.dist.py
@@ -0,0 +1,18 @@
+
+# facebook
+FB_ACCESS_TOKEN = "Hello Girls"
+FB_USER_ID		= "1234567890"
+
+# facecom
+FC_API_KEY		= "1234567890"
+FC_APP_SECRET	= "1234567890"
+
+# mysql
+DB_HOST		= "localhost"
+DB_USER		= "root"
+DB_PSWD		= "root"
+DB_NAME		= "picbrother"
+
+#GEPHI
+GEPHI_HOST	= "localhost"
+GEPHI_PORT	= 8080
